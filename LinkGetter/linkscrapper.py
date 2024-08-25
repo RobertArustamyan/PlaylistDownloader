6a6526698e6d3fe654381ddc1b3a8e88cdf693d5
RobertArustamyan <robertarustamyan2@gmail.com>
2024-08-25 10:18:25 +0400

    Added comments in linkscrapper.py

--- LinkGetter/linkscrapper.py
+++ LinkGetter/linkscrapper.py
@@ -9,6 +9,10 @@ load_dotenv()
 
 
 class LinksParser:
+    """
+    Class that extracts URL links of videos from playlist.
+    NOTE: Playlist must be public!
+    """
     def __init__(self):
         self.options = None
         self.service = None
@@ -17,24 +21,44 @@ class LinksParser:
         self.loadDriver()
 
     def __initializeDriver(self):
+        """
+        Initializes selenium driver and sets settings.
+        """
         self.options = Options()
         self.options.binary_location = '/home/robert/Applications/firefox-127.0b6/firefox/firefox-bin'
         self.service = Service('/usr/bin/geckodriver')
         self.options.add_argument('--headless')
 
     def loadDriver(self):
+        """
+        Loads selenium driver.
+        """
         self.driver = webdriver.Firefox(service=self.service, options=self.options)
 
     def _playlistConnection(self, link):
+        """
+        Send get request to playlist url.
+        :param link: Url of playlist.
+        """
         self.driver.get(link)
 
     def playlistHtml(self, link):
+        """
+        Extracts HTML page of playlist.
+        :param link: Url of playlist.
+        :return: Html page of playlist.
+        """
         self._playlistConnection(link=link)
         self.driver.implicitly_wait(30)
         return self.driver.page_source
 
     @staticmethod
     def getLinks(html):
+        """
+        Extracts URLs from source page.
+        :param html: HTML of page.
+        :return: URL of videos in playlist.
+        """
         soup = BeautifulSoup(html, 'lxml')
         thumbnails = soup.find_all('a', id='thumbnail')
         urls = []
@@ -45,6 +69,11 @@ class LinksParser:
         return urls[1:]
 
     def playlistLinks(self, link):
+        """
+        Connecting all functions of class in one.
+        :param link: Url of playlist.
+        :return: Returns links of videos.
+        """
         plHtml = self.playlistHtml(link=link)
         links = self.getLinks(html=plHtml)
         self.driver.quit()
